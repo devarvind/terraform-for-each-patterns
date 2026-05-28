efa08b98cb7cf370c96fa07797aa33d6c68b3a42
devarvind <arvinddevops17@gmail.com>
2026-05-29 04:14:32 +0530

    Updated Terraform modules and folder structure

--- azurerm_subnet/azurerm_resource_group.tf
+++
@@ -1,6 +0,0 @@
-# resource "azurerm_resource_group" "subnet_rg" {
-#   for_each = var.rg_subnet
-
-#   name     = each.value.name
-#   location = each.value.location
-# }
--- azurerm_virtual_networks/azurerm_resource_group.tf
+++
@@ -1,6 +0,0 @@
-# resource "azurerm_resource_group" "rg_vnet" {
-#   for_each = var.rg_name
-
-#   name     = each.value.name
-#   location = each.value.location
-# }
